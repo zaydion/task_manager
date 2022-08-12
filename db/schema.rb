bb0e124381013b060c9c09d34b76e3c492002b62
Jose Cartagena <jcartagena93@gmail.com>
2022-08-12 12:19:57 -0400

    adds user to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_014022) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_12_161812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_014022) do
     t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", default: 1, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_014022) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "tasks", "users"
 end
