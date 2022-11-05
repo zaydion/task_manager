5b0a42f756b0e29d12755d466d64ead6a843e8a6
Jose Cartagena <jcartagena93@gmail.com>
2022-11-05 16:57:37 -0400

    adds pw to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :images, as: :imageable, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_163434) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_05_205106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_163434) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "users"
